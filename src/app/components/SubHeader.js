7c763fba6cba2dd66f91bf3f93aaae441b8f68f7
NaokiWindows <gtvofromb@gmail.com>
2024-08-06 20:42:11 +0900

    commit

--- src/app/components/SubHeader.js
+++ src/app/components/SubHeader.js
@@ -5,16 +5,16 @@ import Link from "next/link";
 const SubHeader = () => {
     return (
         <div class="h-6 w-full border-b bg-zinc-50 grid grid-cols-4">
-            <div class="flex flex-center">
+            <div class="flex flex-center items-center">
                 <Link href="/" class="w-full border-r text-center text-sm">ホーム</Link>
             </div>
-            <div class="flex flex-center">
+            <div class="flex flex-center items-center">
                 <Link href="/" class="w-full border-x text-center text-sm">投稿する</Link>
             </div>
-            <div class="flex flex-center">
+            <div class="flex flex-center items-center">
                 <Link href="/" class="w-full border-x text-center text-sm">探す</Link>
             </div>
-            <div class="flex flex-center">
+            <div class="flex flex-center items-center">
                 <Link href="/" class="w-full border-x text-center text-sm">トレンド</Link>
             </div>
         </div>
