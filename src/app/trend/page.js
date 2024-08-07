7116e5bcd2b28044d8d5e192203d3fdbd706845e
Naoki <gtvofromb@gmail.com>
2024-08-07 13:30:17 +0900

    commit

---
+++ src/app/trend/page.js
@@ -0,0 +1,15 @@
+'use client'
+
+import Header from "../components/Header";
+import SubHeader from "../components/SubHeader";
+
+const Trend = () => {
+    return (
+        <div>
+           <Header />
+           <SubHeader />
+        </div>
+    )
+}
+
+export default Trend;
