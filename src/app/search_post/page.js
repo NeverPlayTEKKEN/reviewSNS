61152c2950c19d346cb146f2fc6a6bb2f3650229
Naoki <gtvofromb@gmail.com>
2024-08-08 16:44:46 +0900

    commit

--- src/app/search_post/page.js
+++ src/app/search_post/page.js
@@ -2,7 +2,7 @@
 
 import Header from "../components/Header";
 import SubHeader from "../components/SubHeader";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import { MagnifyingGlassIcon, ArrowTrendingUpIcon } from "@heroicons/react/24/solid";
 
 const searchPost = () => {
 
@@ -13,12 +13,21 @@ const searchPost = () => {
         <div class="w-full min-h-screen bg-zinc-50 flex flex-col items-center">
             <Header />
             <SubHeader />
-            <div>
-                <div class="flex justify-center items-center">
-                    <input onChange={handleSearch} class="border"/>
+            <div class="w-2/3 mt-4 flex flex-col justify-center items-center">
+                <div class="flex justify-center">
+                    <input onChange={handleSearch} class="w-full border"/>
                     <MagnifyingGlassIcon class="h-6 w-6" />
                 </div>
+                <Trend />
             </div>
+        </div>
+    )
+}
+
+const Trend = () => {
+    return (
+        <div class="w-full mt-8">
+            <div class="w-full flex items-center justify-start"><ArrowTrendingUpIcon class="w-4 h-4" />トレンド</div>
         </div>
     )
 }
