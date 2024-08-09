2c701ce89eecca6ecd40621e2f4cfc48a2231f24
Naoki <gtvofromb@gmail.com>
2024-08-09 15:11:16 +0900

    commit

--- src/app/components/PostButton.js
+++ src/app/components/PostButton.js
@@ -1,7 +1,8 @@
 'use client'
 import { PencilSquareIcon } from "@heroicons/react/24/outline"
-import { useEffect, useState, useRef } from "react"
-import PostImage from "./PostImage"
+import { useState } from "react"
+import PostWindow from "./PostWindow"
+
 
 const PostButton = () => {
     const [isVisible, setVisible] = useState(false)
@@ -14,7 +15,9 @@ const PostButton = () => {
         <div>
             {isVisible ? (
                 <div onClick={handleClick} class="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center">
-                    <PostWindow />
+                    <div onClick={(event) => {event.stopPropagation()}} class="bg-zinc-50 w-full mx-2">
+                        <PostWindow />
+                    </div>
                 </div>
             ):(
                 <div onClick={handleClick} class="absolute right-8 bottom-16 flex flex-col items-center justify-center bg-amber-200 rounded-full h-20 w-20 shadow-lg">
@@ -24,31 +27,6 @@ const PostButton = () => {
             )}
         </div>
     )
-}
-
-const PostWindow = () => {
-
-    const [ image, setImage ] = useState(null)
-
-    const onFileUpload = (file) => {
-        const upload_file= file
-        if(upload_file){
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                setImage(reader.result);
-                setButtonActive(true)
-            }
-            reader.readAsDataURL(file);
-        }else{
-            alert("no file")
-        }
-    }
-
-    return (
-        <div class="w-full bg-zinc-50">
-            <PostImage onFileUpload={onFileUpload}/>
-        </div>
-    );
 }
 
 export default PostButton;
